77dca77ffb8280afb50e54b66129bf9bec5edd15
hfetoui <hfetoui@hotmail.com>
2022-01-12 23:02:46 +0100

    ajout de composent orm fixture

---
+++ tests/EquipementTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+/**
+ * 
+ */
+class EquipementTest extends TestCase
+{
+	
+	public function testapp(){
+		$this->assertEquals(3,1+2);
+	}
+}
