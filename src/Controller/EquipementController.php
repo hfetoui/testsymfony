278dd476e9609bd23c968cc002557e74d8468643
hfetoui <hfetoui@hotmail.com>
2022-01-12 14:55:47 +0100

    view liste des equippements

--- src/Controller/EquipementController.php
+++ src/Controller/EquipementController.php
@@ -5,16 +5,27 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
+use App\Form\EquipementType;
 
+use App\Entity\Equipement;
+
+/**
+ * @Route("/equipements", name="equipement controller")
+ */
 class EquipementController extends AbstractController
 {
     /**
-     * @Route("/equipement", name="equipement")
+     * @Route("/", name="equipements_liste")
      */
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
+    	$equipements = $doctrine->getRepository(Equipement::class)->findAll();
+ 
         return $this->render('equipement/index.html.twig', [
-            'controller_name' => 'EquipementController',
+            'equipements' => $equipements,
         ]);
     }
+
+    
 }
